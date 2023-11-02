08f03bbb628bf499ceb719b88b13b6be4011bce9
hassanulhaq52 <hassanulhaq52@gmail.com>
2023-11-03 00:29:27 +0500

    Code Refactoring.

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,29 +1,68 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
+//
+// import 'home_screen.dart';
+//
+// class SplashScreen extends StatelessWidget {
+//   const SplashScreen({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//
+//     Future.delayed(const Duration(seconds: 3), () {
+//       Navigator.of(context).pushReplacement(
+//         MaterialPageRoute(builder: (context) => const HomeScreen()),
+//       );
+//     });
+//
+//     return Scaffold(
+//       backgroundColor: Colors.white,
+//       body: Column(
+//         mainAxisAlignment: MainAxisAlignment.center,
+//         children: [
+//           Image.asset(
+//             'images/home.png',
+//             height: MediaQuery.of(context).size.height,
+//             width: MediaQuery.of(context).size.width,
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
 
+
+
+import 'package:flutter/material.dart';
 import 'home_screen.dart';
 
 class SplashScreen extends StatelessWidget {
-  const SplashScreen({super.key});
+  const SplashScreen({Key? key});
 
   @override
   Widget build(BuildContext context) {
-    Future.delayed(const Duration(seconds: 3), () {
-      Navigator.of(context).pushReplacement(
-        MaterialPageRoute(builder: (context) => const HomeScreen()),
-      );
-    });
-
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image.asset(
-            'images/home.png',
-            height: MediaQuery.of(context).size.height,
-            width: MediaQuery.of(context).size.width,
-          ),
-        ],
+      body: FutureBuilder(
+        future: Future.delayed(const Duration(seconds: 3)),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            WidgetsBinding.instance?.addPostFrameCallback((_) {
+              Navigator.of(context).pushReplacement(
+                MaterialPageRoute(builder: (context) => const HomeScreen()),
+              );
+            });
+          }
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'images/home.png',
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+              ),
+            ],
+          );
+        },
       ),
     );
   }
